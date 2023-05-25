53b07e765c007dc178b48a16239c737a720451cc
Pavel Yarashevich <pasha1rab@gmail.com>
2023-05-25 11:37:28 +0200

    remove persitant storage

--- my-app/src/app/store.js
+++ my-app/src/app/store.js
@@ -1,31 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import taskListReducer from '../components/features/TaskList/TaskListSlice'
-import { apiSlice } from '../components/features/TaskList/TaskApi'
-import { setupListeners } from '@reduxjs/toolkit/query'
-import { persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
-import { combineReducers } from '@reduxjs/toolkit'
-
-const persistConfig = {
-  key: 'root',
-  storage: storage,
-  blacklist: ['apiTaskListSlice'],
-}
-
-export const rootReducers = combineReducers({
-  tasksValue: taskListReducer,
-  [apiSlice.reducerPath]: apiSlice.reducer,
-})
-
-const persistedReducer = persistReducer(persistConfig, rootReducers)
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    tasksValue: taskListReducer
+  },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }).concat(apiSlice.middleware),
+    }),
 })
-
-setupListeners(store.dispatch)
-export default store
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,18 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
 
-let persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate persistor={persistor}>
           <App />
-        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
